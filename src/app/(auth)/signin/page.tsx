935d0f56cd9452b876f34f46fb406a574de3dd87
AnanthuTD <ananthutd2021@gmail.com>
2025-04-29 23:07:54 +0530

    feat: added auth check on signin and redirect to dashboard if authenticated

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import { fetchAdminProfileThunk } from "@/actions/adminActions";
 import SignIn from "@/components/authentication/SignIn";
-import React from "react";
+import { useAppDispatch } from "@/redux/hooks";
+import { login, logout } from "@/redux/slices/user";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 function SignInPage() {
+	const router = useRouter();
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		const checkAuth = async () => {
+			const result = await dispatch(fetchAdminProfileThunk() as any);
+			console.log(result);
+			if (result.success) {
+				dispatch(login({ user: result.admin }));
+				router.replace("/dashboard");
+			} else {
+				dispatch(logout());
+			}
+		};
+
+		checkAuth();
+	}, [dispatch, router]);
+
 	return <SignIn />;
 }
 
